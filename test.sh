35c089f09b16cfd664d996ef3d90bf1d0e76a79d
Daniel Freedman <dfreedm@google.com>
2015-02-17 16:47:37 -0800

    Filter output to failing lines
    $PIPESTATUS FTW!

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-npm install editorconfig-tools
+npm install editorconfig-tools > /dev/null
 
-git ls-files | egrep "\.(js|css|json|html)$" | xargs -I '{}' node_modules/.bin/editorconfig-tools check '{}'
+git ls-files | egrep "\.(js|css|json|html)$" | xargs -I '{}' node_modules/.bin/editorconfig-tools check '{}' | awk '$2 == "failed" { print $0 }'
+
+for p in ${PIPESTATUS[@]}; do
+  [ ${p} -ne 0 ] && exit 1
+done
+
+# vim: set ts=2 sw=2 tw=0:
